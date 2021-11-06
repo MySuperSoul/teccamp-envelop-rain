82357c9a91effaa7a213052b24fea88b1fa18f72
xRay2016 <1150722393@qq.com>
2021-11-07 00:26:27 +0800

    [feat] IP limiter

--- middleware/limiterForSafe.go
+++ middleware/limiterForSafe.go
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-06 21:41:01
- * @LastEditTime: 2021-11-07 00:23:40
+ * @LastEditTime: 2021-11-07 00:25:56
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /teccamp-envelop-rain/middleware/limiterForSafe.go
@@ -75,7 +75,7 @@ func NewRateLimiterForIP(interval time.Duration, capacity int64) *RateLimiterMid
 		if key != "" {
 			return key, nil
 		}
-		return "", errors.New("unkown IP")
+		return "", errors.New("unknown IP")
 	}
 	return NewRateLimiterForSafe(interval, capacity, keyGen)
 }
