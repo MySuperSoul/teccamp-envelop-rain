aa4392a5ca272ad8de80f9a8f4a30d38dcba610e
huangyifei <1144358492@qq.com>
2021-11-13 16:55:58 +0800

    feat: Add snow flake test in script

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@
  # @FilePath: /teccamp-envelop-rain/tmp.sh
 ### 
 
-go test -c  common/util_test.go common/util.go && ./common.test
+go test -c  common/util_test.go common/util.go common/snowflake.go common/snowflake_test.go && ./common.test
 
 go test -c  repository/mysql_test.go repository/redis_test.go repository/redis.go repository/mysql.go repository/model.go && ./repository.test
 
